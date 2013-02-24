51e6e499752691c88ba51b03acc78780dd3060e1
vinodkd <vinod@vinodkd.org>
2013-02-24 15:29:08 +0530

    refactored generate.rb to 1.break down the generate sub steps, 2. recursively generate child architecture diagrams, 3. correct the filenames in the image generation process

--- lib/generate.rb
+++ lib/generate.rb
@@ -6,7 +6,6 @@ module Sid
     def initialize(output_file,output)
       @output_file = output_file
       @output = output
-      @gen_arch_img = (@output["to-build"][:has_architecture])? true : false
       setup "gen_root(root)",'sid.erb'
       setup "gen_features(root)","gen_features.erb"
       setup "gen_capabilities(root)","gen_capabilities.erb"
@@ -28,23 +27,49 @@ module Sid
     end
     
     def generate
+      gen_main_file
+      gen_css_file
+      gen_arch_images @output["to-build"]
+    end
+
+    def gen_main_file
       File.open(@output_file,"w+") do |outfile|
         outfile.puts gen_root @output
       end
+    end
       
+    def gen_css_file
       css_source_file = File.join(File.dirname(__FILE__),"sid.css")
       css_output_file = File.join(File.dirname(@output_file),'sid.css')
       FileUtils.cp css_source_file, css_output_file
-      
-      if @gen_arch_img
-        gv_output_file = @output_file.sub '.html','.dot'
-        File.open(gv_output_file, "w+") do |gvfile|
-          gvfile.puts gen_arch_img @output
-        end
-        png_output_file = @output_file.sub '.html', '.png'
-        dot_done = `dot #{gv_output_file} | neato -n -s -Tpng -o#{png_output_file}`
-      end      
     end
+
+    def gen_arch_images(root)
+      gen_arch_img_reqd = (root[:has_architecture])? true : false
+        
+      if gen_arch_img_reqd
+        gen_one_arch_image root
+      end
+      if root[:has_child_definitons]
+        root['to-build'].each do | child|
+          child.each do |key,val|
+            #puts "gen key: #{key}"
+            next if !root[:new_components].has_key? key # this is not a declared component
+            gen_arch_images val
+          end 
+        end
+      end
+    end
+
+    def gen_one_arch_image(root)
+        #puts "genone arch ref : #{root[:arch_ref]}"
+        gv_output_file = root[:arch_ref] + '.dot'
+        File.open(gv_output_file, "w+") do |gvfile|
+          gvfile.puts gen_arch_img root
+        end
+        png_output_file = gv_output_file.sub '.dot', '.png'
+        dot_done = `dot #{gv_output_file} | neato -n -s -Tpng -o#{png_output_file}`
+      end
   end
 end
 
